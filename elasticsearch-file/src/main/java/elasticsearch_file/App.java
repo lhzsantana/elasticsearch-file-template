dab2d806285bba976ed92b90bd826a37c807979f
luiz <luiz@luiz>
2015-04-02 09:11:09 -0300

    Initial commit

---
+++ elasticsearch-file/src/main/java/elasticsearch_file/App.java
@@ -0,0 +1,13 @@
+package elasticsearch_file;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
